442efb81d996988710935a74b10ae74d9f040423
Redon <Redon.Bytyqi@student.cacttus.education>
2024-12-24 11:16:31 -0800

    .

--- DSTHEME/index.php
+++ DSTHEME/index.php
@@ -2,7 +2,27 @@
 
     get_header();
 
+?>
 
+
+<?php
+
+    if(have_posts() ):
+        while(have_posts()): the_post();
+
+?>
+
+  <h1>Ketu do te vendosen postet</h1>
+
+<?php
+    
+    endwhile;
+    endif;
+?>
+
+
+
+<?php
     get_footer();
 
 
