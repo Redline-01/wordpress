fcb2fac9d3781f1950e061b3d1a052c82be3db0c
Redon <Redon.Bytyqi@student.cacttus.education>
2025-01-14 11:41:40 -0800

    .

--- DSTHEME/functions.php
+++ DSTHEME/functions.php
@@ -3,8 +3,11 @@
    function add_script(){
 
     wp_enqueue_style("style", get_template_directory_uri()."./style.css" , false , "1.0" , "all");
-    wp_enqueue_script("main", get_template_directory_uri()."/js/main.js" , array('jquery') , "1.0");
     wp_enqueue_style("main", get_template_directory_uri()."/css/main.css" , false , "1.0", "all");
+    wp_enqueue_style("bootstrap.min", get_template_directory_uri()."/css/bootstrap.min.css" , false , "1.0" , "all");
+
+    wp_enqueue_script("main", get_template_directory_uri()."/js/main.js" , array('jquery') , "1.0");
+    wp_enqueue_script("bootstrap.bundle.min", get_template_directory_uri()."/js/bootstrap.bundle.min.js" , array('jquery') , "1.0");
 
    }
 
