e3702b0517ddbc2e259a647631e775615df65ddd
Redon <Redon.Bytyqi@student.cacttus.education>
2025-01-21 11:29:36 -0800

    .

--- DSTHEME/index.php
+++ DSTHEME/index.php
@@ -30,7 +30,7 @@ get_header();
 
 
 <div class="container" style="display:flex;">
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 300px; padding: 10px;">
   <img src="<?php bloginfo("template_directory");?> ./images/4.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
@@ -39,16 +39,16 @@ get_header();
   </div>
 </div>
 
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 300px; padding: 10px">
   <img src="<?php bloginfo("template_directory");?> ./images/5.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
-</div
+</div>
 
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 300px; padding: 10px">
   <img src="<?php bloginfo("template_directory");?> ./images/6.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
@@ -57,5 +57,14 @@ get_header();
   </div>
 </div>
 </div>
+
+<br>
+<br>
+
+<?php
+
+get_sidebar();
+
+?>
 
 <?php get_footer(); ?> 
