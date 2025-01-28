a556d79f52a4c87c8d001110ea6e1f66efc391cb
Redon <Redon.Bytyqi@student.cacttus.education>
2025-01-28 10:55:47 -0800

    .

--- DSTHEME/single.php
+++ DSTHEME/single.php
@@ -5,8 +5,31 @@
 ?>
 
 
+<div class="container">
+
+     
+      <?php
+
+      if(have_posts()):
+        while(have_posts()):the_post();
+
+
+      ?>
+
+      
+
+
+
+</div>
+
+
+
+
+
+
+
 <?php
 
    get_footer();
-   
+
 ?>   
