5e1d6f1f3d7a1cf46325cc02cdbea12d33af13f7
Redon <Redon.Bytyqi@student.cacttus.education>
2025-01-30 11:22:58 -0800

    .

--- DSTHEME/search.php
+++ DSTHEME/search.php
@@ -6,7 +6,7 @@
 
  <div class="row">
 
-  <!-- <?php echo get_search_query(); ?>  shows what we typed in search --> 
+   <?php get_search_query(); ?> 
 
   <?php
 
@@ -14,10 +14,33 @@
     while(have_posts()): the_post();
 
   ?>
-  <h1><?php the_title(); ?></h1>
+  <div class="card mb-3">
+
+    <?php
+      
+      if(has_post_thumbnail()){ ?>
+
+      <img style="width:200px; height:200px;" src="<?php wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>" alt="">
+
+      <?php
+      } else{
+      ?>
+
+<img style="width:200px; height:200px;" src="<?php bloginfo("template_directory");?> ./images/1.png" alt="">
+
+
+
+<?php
+      }
+?>
+
+  </div>
+
+
+ <!-- <h1><?php the_title(); ?></h1>
   <p><?php the_content(); ?></p>
   <hr>
-  <small><?php the_time(); ?> </small>
+  <small><?php the_time(); ?> </small> -->
 
 
 <?php
