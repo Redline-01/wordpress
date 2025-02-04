c41fecaa7e5902a3981e895e25f98a6fd9ae8a2a
Redon <Redon.Bytyqi@student.cacttus.education>
2025-02-04 11:15:22 -0800

    .

--- DSTHEME/paginator.php
+++ DSTHEME/paginator.php
@@ -3,6 +3,23 @@
    if(have_post()):
 
     while(have_post()): the_post();
+
+
+   endwhile;
     
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="nav-previous alignleft"><?php next_posts_link( 'older posts' );  ?></div>
+    <div class="nav-next alignright"><?php previous_posts_link( 'Newer Posts' ) ?></div>
+
+    
+</body>
+</html>
