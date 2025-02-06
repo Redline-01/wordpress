642baf80d713f4220f99241e2b612b6a06a0cdcd
Redon <Redon.Bytyqi@student.cacttus.education>
2025-02-06 10:55:22 -0800

    404 page

--- DSTHEME/404.php
+++ DSTHEME/404.php
@@ -7,22 +7,28 @@ get_header();
 
 
 
-    <div id="primary" class="content-area">
-        <div id="content" class="site-content" role="main">
-            <header class="page-header">
-                <h1 class="page-title"><?php _e( 'Not found', 'twentythirteen' ) ?></h1>
-            </header>
+    <div class="container">
+        <div class="row">
 
-            <div class="page-wrapper">
-                <div class="page-content">
-                    <h2><?php _e( 'This is awesome', 'twentythirteen' ) ?></h2>
-                    <p><?php _e( 'It looks like nothing was found at this location.Maybe try a search?', 'twentythirteen' ) ?></p>
-                </div>
-                <?php get_search_form(); ?>
+        <div class="col-8">
 
-            </div>
+        <h1>Page not found</h1>
+
+            <img src="<?php bloginfo('template_url'); ?>/images/404.gif" alt="">
+
+        </div>
+
+        <div class="col-4">
+            
+        <?php get_search_form(); ?>
+
+        </div>
+
         </div>
     </div>
+          
+
+       
 
 <?php
 
